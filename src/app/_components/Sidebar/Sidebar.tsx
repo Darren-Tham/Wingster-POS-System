558e6f38c4a3c6ee6e7ab7bdd694cc6ff60f68b5
Darren Tham <darrent9859@gmail.com>
2023-08-20 15:16:35 -0400

    Update size of icons

--- src/app/_components/Sidebar/Sidebar.tsx
+++ src/app/_components/Sidebar/Sidebar.tsx
@@ -14,14 +14,14 @@ import Logout from '../../../../public/logout-icon.svg'
 
 export default function Sidebar(): JSX.Element {
 	return (
-		<aside className='flex flex-col justify-between items-center w-max h-screen px-2 py-6'>
-            <div className='flex flex-col items-center gap-12'>
+		<aside className='flex flex-col justify-between items-center w-max h-screen px-4 py-8'>
+            <div className='flex flex-col items-center gap-16'>
                 <Image width="70" height="70" src={WingsterIcon} alt='Wingster Icon' unoptimized />
                 <Image src={HouseIcon} alt='House Icon' />
                 <Image src={BowlIcon} alt='Bowl Icon' />
                 <Image src={DollarSign} alt='Dollar Sign Icon' />
                 <Image src={CashRegister} alt='Cash Register Icon' />
-                <span className="text-3xl text-icon-color">ENG</span>
+                <span className="text-xl text-icon-color">ENG</span>
             </div>
 			<Image src={Logout} alt='Logout Icon' />
 		</aside>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,8 @@
 import './globals.css'
 import type { ReactNode } from 'react'
-import { Open_Sans, Inter } from 'next/font/google'
+import { Open_Sans } from 'next/font/google'
 
 const openSans = Open_Sans({ subsets: ['latin'] })
-const inter = Inter({ subsets: ['latin'] })
 
 export default function RootLayout({ children }: { children: ReactNode }) {
 	return (
