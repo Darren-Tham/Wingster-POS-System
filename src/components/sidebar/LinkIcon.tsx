1cc8208136bb9475cf260503a63adcae9974f6bf
mmm29992 <121207758+mmm29992@users.noreply.github.com>
2023-08-24 14:05:08 -0400

    Created the LinkIcon component

--- src/components/sidebar/LinkIcon.tsx
+++ src/components/sidebar/LinkIcon.tsx
@@ -1,8 +1,36 @@
+// Next
+import Link from "next/link";
+import Image, { StaticImageData } from "next/image";
+
+// Components 
+import SelectionBar from "./SelectionBar";
+
+type Props = {
+  href: string;
+  src: StaticImageData;
+  alt: string;
+  iconStyles: string;
+  isSelected: boolean;
+};
 
 /**
  * Link element with Icon that links to
  * another page in `Sidebar`.
+ * 
+ * @param Props `LinkIcon` props
+ * @returns `LinkIcon` component
  */
-export default function LinkIcon() {
-   
+export default function LinkIcon({
+  href,
+  src,
+  alt,
+  iconStyles,
+  isSelected,
+}: Props) {
+    return (
+        <Link className={iconStyles} href={href}>
+            <Image src={src} alt={alt} />
+            {isSelected && <SelectionBar/>}
+        </Link>
+    )
 }
