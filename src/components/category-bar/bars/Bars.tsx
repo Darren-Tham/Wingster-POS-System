c3f1a8910a354d8fd34eafec978d932b5dc1bcf8
Darren Tham <darrent9859@gmail.com>
2023-09-02 13:11:50 -0400

    Make CategoryBar scrollable

--- src/components/category-bar/bars/Bars.tsx
+++ src/components/category-bar/bars/Bars.tsx
@@ -1,3 +1,9 @@
+"use client"
+
+// React
+import { useRef, type MutableRefObject } from "react"
+import { useDraggable } from "react-use-draggable-scroll"
+
 // Components
 import Bar from "./Bar"
 
@@ -8,8 +14,11 @@ import Bar from "./Bar"
  * @returns `Bars` component
  */
 export default function Bars() {
+  const ref = useRef<HTMLDivElement>() as MutableRefObject<HTMLDivElement>
+  const { events } = useDraggable(ref)
+
   return (
-    <div className="flex overflow-x-auto">
+    <div className="flex overflow-x-auto" {...events}  ref={ref}>
       <Bar name="All" />
       <Bar name="Appetizer" />
       <Bar name="Entrees" />
